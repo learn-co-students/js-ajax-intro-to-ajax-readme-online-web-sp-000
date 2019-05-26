28d4287fc1f6d8c2ffae026705594ce23c986a48
Joe Ganobsik <jganobsik@gmail.com>
2019-05-26 17:04:12 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,56 @@
 // your code here
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+[
+  {
+    id: 18221276,
+    name: 'git-consortium',
+    full_name: 'octocat/git-consortium',
+    owner: {
+      login: 'octocat',
+      id: 583231,
+      avatar_url: 'https://avatars.githubusercontent.com/u/583231?v=3',
+      gravatar_id: '',
+      url: 'https://api.github.com/users/octocat',
+      html_url: 'https://github.com/octocat',
+      followers_url: 'https://api.github.com/users/octocat/followers',
+      following_url:
+        'https://api.github.com/users/octocat/following{/other_user}',
+      gists_url: 'https://api.github.com/users/octocat/gists{/gist_id}',
+      starred_url:
+        'https://api.github.com/users/octocat/starred{/owner}{/repo}',
+      subscriptions_url: 'https://api.github.com/users/octocat/subscriptions',
+      organizations_url: 'https://api.github.com/users/octocat/orgs',
+      repos_url: 'https://api.github.com/users/octocat/repos',
+      events_url: 'https://api.github.com/users/octocat/events{/privacy}',
+      received_events_url:
+        'https://api.github.com/users/octocat/received_events',
+      type: 'User',
+      site_admin: false
+    }
+  }
+  //... more!
+];
+
+
+
+
+function showRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
+    document.getElementById('repositories').innerHTML = repoList;
+}
+ 
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
