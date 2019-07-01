1937fc9f695d45f628e7b6b4b7d7b1f73ba25583
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 22:00:47 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
 // your code here
+function getRepositories(){
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('get', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+function showRepositories() {
+  const repos = JSON.parse(this.responseText)
+  let repoList = `<ul>${repos.map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
+
+  document.getElementById("repositories").innerHTML = repoList;
+}
+
+function getCommits(anchor) {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open("get", `https://api.github.com/repos/octocat/${anchor.dataset.repo}/commits`);
+  req.send();
+}
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+      )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
