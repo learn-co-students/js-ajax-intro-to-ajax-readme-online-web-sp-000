ca96c1ec248c42e2eecb2e94398b7b618a8c0817
Andrew Davisson <akdavisson4@gmail.com>
2019-09-19 19:02:30 -0600

    edits

--- js/index.js
+++ js/index.js
@@ -1,44 +1,50 @@
 // your code here
-function showRepositories(){
+function showRepositories() {
   let repos = JSON.parse(this.responseText);
   console.log(repos);
   const repoList = `<ul>${repos
     .map(
-      r => '<li>' + 
-      r.name + 
-      ' - <a href="#" data-repo="' +
-      r.name +
-      '" onclick="getCommits(this)">GetCommits</a></li>'
-    ).join('')}</ul>`;
+      r =>
+        "<li>" +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">GetCommits</a></li>'
+    )
+    .join("")}</ul>`;
 
-  document.getElementById('repositories').innerHTML = repoList;
+  document.getElementById("repositories").innerHTML = repoList;
 }
 
 function getRepositories() {
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
-  req.open('GET', 'http://api.github.com/users/adavisson/repos');
+  req.addEventListener("load", showRepositories);
+  req.open("GET", "http://api.github.com/users/adavisson/repos");
   req.send();
 }
 
 function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showCommits);
-  req.open('GET', 'https://api.github.com/repos/adavisson/' + name + '/commits');
+  req.addEventListener("load", showCommits);
+  req.open(
+    "GET",
+    "https://api.github.com/repos/adavisson/" + name + "/commits"
+  );
   req.send();
 }
 
-function showCommits(){
+function showCommits() {
   const commits = JSON.parse(this.responseText);
   const commitList = `<ul>${commits
-  .map(
-    commit =>
-    '<li><strong>' +
-    commit.author.login +
-    '</strong> - ' +
-    commit.commit.message +
-    '</li>'
-  ).join('')}</ul>`;
-  document.getElementById('commits').innerHTML = commitList;
+    .map(
+      commit =>
+        "<li><strong>" +
+        commit.author.login +
+        "</strong> - " +
+        commit.commit.message +
+        "</li>"
+    )
+    .join("")}</ul>`;
+  document.getElementById("commits").innerHTML = commitList;
 }
