60b70a15c3c92973500645e6edb64d7f0b79c92b
Alan Krajina <alankrajina88@gmail.com>
2019-09-23 10:48:23 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // your code here
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+function showRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
